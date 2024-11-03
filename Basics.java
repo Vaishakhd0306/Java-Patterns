3f05d5a02fb387f3dae957033ab892a721ae7dd6
Vaish <vaishakhd0306@gmail.com>
2024-11-03 12:20:12 +0530

    age

--- Basics.java
+++ Basics.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
+class Test {
+    public void temp() {
+        System.out.println("hello world");
+    }
+}
+
 public class Basics {
     public static void main(String[] args) {
-        System.out.println("hello world123");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("write the age");
+        int age = sc.nextInt();
+        if (age >= 18) {
+            System.out.println("Adult");
+
+        } else {
+            System.out.println("Teen");
+        }
+        sc.close();
+        Test ts = new Test();
+        ts.temp();
     }
+
 }
